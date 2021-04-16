b25f83f1cf2fc6ee4af32c4589b4a765c4524be8
sondn99 <ngocsoncn532.ptit@gmail.com>
2021-04-16 11:41:41 +0700

    Initial Commit

---
+++ MovieAPP2_demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieAPP2_demo
+//
+//  Created by ngoc son on 16/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
